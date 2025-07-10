a91c6f7b92f4d0d46adfacb6918e2530092c85f2
MamunCh0wdhury <189634349+asrhospitals@users.noreply.github.com>
2025-07-11 02:56:23 +0530

    DB Connect Setup

--- db/db.js
+++ db/db.js
@@ -1,11 +1,15 @@
 /// Currently Server is running on localhost:5432
-
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('pharmacyDb', 'postgres', 'Postgres123', {
-    host: 'localhost',
+// const sequelize = new Sequelize('pharmacyDb', 'postgres', 'Postgres123', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     port: 5432,
+//   });
+
+  const sequelize= new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    port: 5432,
-  });
+});
 
   module.exports = sequelize;
  
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const sequelize = require("./db/db");
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 const MasterRoutes = require("./routes/master/masterroutes");
 
+
+// Server Test Route
+app.get('/',async (req,res) => {
+    return res.json({message:"Server up and running"});
+});
 
 
 // Routes for Masters
@@ -19,8 +25,8 @@ const MasterRoutes = require("./routes/master/masterroutes");
 const startServer = async () => {
   try {
     await sequelize.authenticate().then(() => { console.log("Db connected to the localHost");}).catch((err) => {console.log("Error connecting to the Db", err);});
-    // await sequelize.sync();
-    app.listen(port, () => {console.log(`Server is running on port ${port}`);})} catch (error) {console.log(error);}
+    //  await sequelize.sync();
+    app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);})} catch (error) {console.log(error);}
 };
 
 startServer();
