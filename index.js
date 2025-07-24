bd5dd16867d8260dfd942e76b1c1a81298913d3d
asrhospitals <189634349+asrhospitals@users.noreply.github.com>
2025-07-24 19:37:57 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,41 +7,39 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 const MasterRoutes = require("./routes/master/masterroutes");
-const AuthRoutes = require("./routes/auth/auth");
-const verifyToken = require("./middleware/authMiddleware");
-const role = require("./middleware/roleMiddleware");
+const AuthRoutes=require('./routes/auth/auth');
+const verifyToken=require('./middleware/authMiddleware');
+const role =require('./middleware/roleMiddleware');
 
 // Server Test Route
-app.get("/", async (req, res) => {
-  return res.json({ message: "Pharmacy server is running" });
+app.get('/',async (req,res) => {
+    return res.json({message:"Pharmacy server is running"});
 });
 
+
 // Routes for Authentication
-app.use("/pharmacy/auth", AuthRoutes);
+app.use("/pharmacy/auth",AuthRoutes);
+
+
 
 // Routes for Masters
-app.use("/pharmacy/admin/master", verifyToken, role("admin"), MasterRoutes);
+app.use("/pharmacy/admin/master",verifyToken,role('admin'), MasterRoutes);
+
 
 // For LocalHost Test
 //app.use("/pharmacy/master", MasterRoutes);
 
+
+
+
+
 const startServer = async () => {
   try {
-    await sequelize
-      .authenticate()
-      .then(() => {
-        console.log("Db Connected");
-      })
-      .catch((err) => {
-        console.log("Error connecting to the Db", err);
-      });
+    await sequelize.authenticate().then(() => { console.log("Db Connected");}).catch((err) => {console.log("Error connecting to the Db", err);});
     // await sequelize.sync();
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);})} catch (error) {console.log(error);}
 };
 
 startServer();
+
+
