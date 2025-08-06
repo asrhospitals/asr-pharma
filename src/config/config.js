26e78ddd7262bc46a0ae76ee16eee97dfdfe4a7d
Parivesh09 <rimjha.parivesh2002@gmail.com>
2025-08-06 15:17:05 +0530

    feat: Refactor database configuration to use config.js and environment variables
    to make the deployed backend working

---
+++ src/config/config.js
@@ -0,0 +1,29 @@
+require('dotenv').config(); // only if needed (for local dev)
+
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "Parivesh@09",
+    database: "pharmacydb",
+    host: "localhost",
+    port: 5432,
+    dialect: "postgres",
+    logging: false,
+  },
+  test: {
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "Parivesh@09",
+    database: process.env.DB_NAME || "pharmacydb",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+  },
+};
