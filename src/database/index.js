3a5ff31c2352db1bedae2a47042f999645a6c9aa
Parivesh09 <rimjha.parivesh2002@gmail.com>
2025-08-06 15:04:03 +0530

    testing if it's working in production or not

--- src/database/index.js
+++ src/database/index.js
@@ -1,31 +1,42 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  console.log(
+    `Connecting to database using environment variable: ${
+      process.env[config.use_env_variable]
+    }`
+  );
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+  console.log(`Connecting to database: ${JSON.stringify(config)}`);
 }
 
 function loadModelsFromDir(dirPath) {
-  fs.readdirSync(dirPath).forEach(file => {
+  fs.readdirSync(dirPath).forEach((file) => {
     const fullPath = path.join(dirPath, file);
     if (fs.statSync(fullPath).isDirectory()) {
       loadModelsFromDir(fullPath);
     } else if (
-      file.indexOf('.') !== 0 &&
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     ) {
       const model = require(fullPath)(sequelize, Sequelize.DataTypes);
       db[model.name] = model;
@@ -33,9 +44,9 @@ function loadModelsFromDir(dirPath) {
   });
 }
 
-loadModelsFromDir(path.join(__dirname, '../models'));
+loadModelsFromDir(path.join(__dirname, "../models"));
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
