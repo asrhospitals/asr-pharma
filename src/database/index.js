8632edf350183ab976f448d63200071911c56438
Parivesh09 <rimjha.parivesh2002@gmail.com>
2025-08-06 15:53:09 +0530

    feat: Simplify database connection logic and streamline model loading in index.js; update deploy.yml to create .env file for environment variables

--- src/database/index.js
+++ src/database/index.js
@@ -3,48 +3,27 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
-const process = require("process");
 const basename = path.basename(__filename);
+require('dotenv').config();
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  console.log(
-    `Connecting to database using environment variable: ${
-      process.env[config.use_env_variable]
-    }`
-  );
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-  console.log(`Connecting to database: ${JSON.stringify(config)}`);
-}
+const sequelize = config.use_env_variable
+  ? new Sequelize(process.env[config.use_env_variable], config)
+  : new Sequelize(config.database, config.username, config.password, config);
 
-function loadModelsFromDir(dirPath) {
-  fs.readdirSync(dirPath).forEach((file) => {
-    const fullPath = path.join(dirPath, file);
-    if (fs.statSync(fullPath).isDirectory()) {
-      loadModelsFromDir(fullPath);
-    } else if (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".js" &&
-      file.indexOf(".test.js") === -1
-    ) {
-      const model = require(fullPath)(sequelize, Sequelize.DataTypes);
-      db[model.name] = model;
-    }
+fs.readdirSync(path.join(__dirname, "../models"))
+  .filter((file) =>
+    file.endsWith(".js") && file !== basename && !file.endsWith(".test.js")
+  )
+  .forEach((file) => {
+    const model = require(path.join(__dirname, "../models", file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
   });
-}
-
-loadModelsFromDir(path.join(__dirname, "../models"));
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
